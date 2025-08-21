d15508a00cce74603501ce32fba549ecd7bda078
hosterion-pl <a.zug@hosterion.pl>
2025-08-21 11:41:07 +0200

    Add files via upload

---
+++ examples/list_segments.py
@@ -0,0 +1,43 @@
+from setup_api import setup
+from emailon.endpoint.list_segments import ListSegments
+
+"""
+SETUP THE API
+"""
+setup()
+
+"""
+CREATE THE ENDPOINT
+"""
+endpoint = ListSegments()
+
+
+"""
+GET ALL SEGMENTS OF A LIST
+"""
+response = endpoint.get_segments(list_uid='LIST_UID')
+
+"""
+DISPLAY RESPONSE
+"""
+print(response.content)
+
+"""
+GET ONE ITEM
+"""
+response = endpoint.get_segment(list_uid='LIST_UID', segment_uid='SEGMENT_UID')
+
+"""
+DISPLAY RESPONSE
+"""
+print(response.content)
+
+"""
+GET ALL SUBSCRIBERS OF A LIST SEGMENT
+"""
+response = endpoint.get_subscribers(list_uid='LIST_UID', segment_uid='SEGMENT_UID', page=1, per_page=10)
+
+"""
+DISPLAY RESPONSE
+"""
+print(response.content)
